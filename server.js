26ffdeb5c52e397789117f2d3b4e6fa4a599b130
KAPCHA <kapchabeat@gmail.com>
2017-02-15 22:19:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/My-Profile', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'My Profile.html'));
+app.get('/Profile', function (req, res) {
+    res.send(path.join(__dirname, 'ui', 'My Profile.html'));
 });
 
 app.get('/2', function (req, res) {
