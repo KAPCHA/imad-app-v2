41f3766a1d46b21e549e4cd90eb5fbc9df001176
KAPCHA <kapchabeat@gmail.com>
2017-02-22 17:35:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,6 +91,9 @@ app.get('/:page', function (req, res) {
   res.send(createTemplate(pages[page]));
 });
 
+app.get('/main.js', function (req, res) {
+res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
