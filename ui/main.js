4b8127158c8903913d90256f576e25a23b5d823e
KAPCHA <kapchabeat@gmail.com>
2017-02-24 02:16:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,8 @@ var element = document.getElementById('1');
 element.innerHTML='newwwwwwwww';
 var element = document.getElementById('2');
 element.innerHTML='WOW';
+
+var img = document.getElementById('url');
+img.onclick = function (){
+   img.style.margiLeft = '100px'
+};
