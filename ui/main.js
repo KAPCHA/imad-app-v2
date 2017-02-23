5af4d1fc071547097c4c2fad6b4e9a0cdbd37a5f
KAPCHA <kapchabeat@gmail.com>
2017-02-24 02:25:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,5 @@
 console.log('Loaded!');
 
-var element = document.getElementById('1');
-element.innerHTML='newwwwwwwww';
-var element = document.getElementById('2');
-element.innerHTML='WOW';
-
 var img = document.getElementById('url');
 img.onclick = function (){
    img.style.marginLeft = '100px';
